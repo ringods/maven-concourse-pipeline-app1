3404474d7309fce7795247be7f8bb1500fbfa670
Marcial Rosales <mrosales@pivotal.io>
2017-02-10 18:36:03 +0100

    rename wrong var

--- vars.tf
+++ vars.tf
@@ -3,7 +3,6 @@
 variable "CF_API_URL" {}
 variable "CF_ADMIN_USER" {}
 variable "CF_ADMIN_PASSWORD" {}
-variable "CF_UAA_ADMIN_USER" {}
 variable "CF_UAA_ADMIN_CLIENT_ID" {}
 variable "CF_UAA_ADMIN_CLIENT_SECRET" {}
 variable "CF_SKIP_SSL_VALIDATION" {}
