82f8dcf90179c698282a149347853478ac23a66c
Marcial Rosales <mrosales@pivotal.io>
2017-02-10 18:30:27 +0100

    rename all variables

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 # Configure the CloudFoundry Provider
 
 provider "cf" {
-    api_url = "${var.api_url}"
-    user = "admin"
+    api_url = "${var.CF_API_URL}"
+    user = "${var.CF_ADMIN_USER}"
     password = "${var.CF_ADMIN_PASSWORD}"
-    uaa_client_id = "admin"
-    uaa_client_secret = "${var.uaa_admin_client_secret}"
-    skip_ssl_validation = true
+    uaa_client_id = "${var.CF_UAA_ADMIN_CLIENT_ID}"
+    uaa_client_secret = "${var.CF_UAA_ADMIN_CLIENT_SECRET}"
+    skip_ssl_validation = "${var.CF_SKIP_SSL_VALIDATION}"
 }
--- vars.tf
+++ vars.tf
@@ -1,12 +1,13 @@
 # Cloud Foundry Credentials
 
-variable "api_url" {}
-variable "admin_user" {}
+variable "CF_API_URL" {}
+variable "CF_ADMIN_USER" {}
 variable "CF_ADMIN_PASSWORD" {}
-variable "uaa_admin_user" {}
-variable "uaa_admin_client_secret" {}
+variable "CF_UAA_ADMIN_USER" {}
+variable "CF_UAA_ADMIN_CLIENT_ID" {}
+variable "CF_UAA_ADMIN_CLIENT_SECRET" {}
+variable "CF_SKIP_SSL_VALIDATION" {}
 
-# S3 remote state 
+# S3 remote state
 variable "S3_BUCKET" {}
 variable "S3_KEY" {}
-
