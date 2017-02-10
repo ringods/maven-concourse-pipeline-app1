28e9ca91d18672bb5b043273ac61799e5af38137
Marcial Rosales <mrosales@pivotal.io>
2017-02-10 18:26:21 +0100

    rename vars

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@
 provider "cf" {
     api_url = "${var.api_url}"
     user = "admin"
-    password = "${var.ADMIN_PASSWORD}"
+    password = "${var.CF_ADMIN_PASSWORD}"
     uaa_client_id = "admin"
     uaa_client_secret = "${var.uaa_admin_client_secret}"
     skip_ssl_validation = true
--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@
 
 variable "api_url" {}
 variable "admin_user" {}
-variable "ADMIN_PASSWORD" {}
+variable "CF_ADMIN_PASSWORD" {}
 variable "uaa_admin_user" {}
 variable "uaa_admin_client_secret" {}
 
