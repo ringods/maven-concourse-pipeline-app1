af9d96080c63fe22591c0a87b25c71348f325fe1
Marcial Rosales <mrosales@pivotal.io>
2017-02-10 17:58:33 +0100

    add variables files

---
+++ vars.tf
@@ -0,0 +1,12 @@
+# Cloud Foundry Credentials
+
+variable "api_url" {}
+variable "admin_user" {}
+variable "admin_password" {}
+variable "uaa_admin_user" {}
+variable "uaa_admin_client_secret" {}
+
+# S3 remote state 
+variable "s3_bucket" {}
+variable "s3_key" {}
+
