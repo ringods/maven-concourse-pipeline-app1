c6ecf3e8fdad8eb49e3761a77ee91dba4cd70279
Marcial Rosales <mrosales@pivotal.io>
2017-02-14 18:58:03 +0100

    added user provided service to redirect logs

--- services.tf
+++ services.tf
@@ -2,7 +2,7 @@ data "cf_service" "redis" {
     name = "p-redis"
 }
 data "cf_service_plan" "redis" {
-    name = "shared-vm" 
+    name = "shared-vm"
     service = "${data.cf_service.redis.id}"
 }
 
@@ -11,4 +11,10 @@ output "redis_id" {
 }
 output "redis_plan_id" {
   value = "${data.cf_service_plan.redis.id}"
+}
+
+resource "cf_user_provided_service" "syslog" {
+	name = "syslog"
+  space = "${cf_space.dev.id}"
+  syslogDrainURL = "syslog://logs3.papertrailapp.com:28020"
 }
